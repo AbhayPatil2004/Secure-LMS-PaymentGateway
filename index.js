510d981462320318bfec5a513b54e4310de07bfa
Abhay Ramkrushna Patil <patilabhay484@gmail.com>
2025-10-07 00:39:12 +0530

    Basic Setup dome

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // require("dotenv").config()
 import dotenv from "dotenv"
+import express from "express"
 
 dotenv.config();
 console.log("Hello world")
 console.log(process.env.PORT)
+
+const app = express()
+const PORT = process.env.PORT
+
+app.listen( PORT , () => {
+    console.log("Server is Running ")
+})
